53ed9d1f43557edb35805195903b5a25fbfd47d0
Emil <eybatove23@sch57.ru>
2021-11-24 22:07:18 +0300

    my commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import uic
+import sqlite3
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtCore import Qt
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+
+        con = sqlite3.connect('films_db.sqlite')
+        cur = con.cursor()
+        result = list(cur.execute(f"""SELECT * FROM info""").fetchall())
+        con.close()
+        for i, row in enumerate(result):
+            self.table.setRowCount(self.table.rowCount() + 1)
+            for j, elem in enumerate(row):
+                item = QTableWidgetItem(str(elem))
+                item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)
+                self.table.setItem(i, j, item)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
